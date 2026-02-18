3a294359f1535eeb4d81a289768aeb1a51146946
franngo <kitofran15@gmail.com>
2026-02-18 01:56:53 -0300

    Se implementa la subtarea que procesa el input del usuario y delega de la forma correspondiente según cuál sea dicho input.

--- java-console-minesweeper/src/board/ConsoleBoard.java
+++ java-console-minesweeper/src/board/ConsoleBoard.java
@@ -11,7 +11,8 @@ public class ConsoleBoard {
     }
 
     public void startGame() {
-        System.out.println("");
+        System.out.println("Welcome to Minesweeper! You can use the commands clear, flag, and unflag. Next to" +
+                "one of these commands, you must enter the row and the column number. For example: clear 5-7");
         while(board.getIsActive()) {
             //mostrar en pantalla el tablero
             board.showBoardOnConsole();
@@ -24,6 +25,44 @@ public class ConsoleBoard {
     }
 
     private void processInput(String input) {
+        String[] parts = input.split(" ");
+
+        //Input must have only 2 parts
+        if (parts.length != 2) {
+            System.out.println("Invalid input format. Inputs must have only 2 parts: command and row-column " +
+                    "(For example: clear 6-2)");
+            return;
+        }
+
+        if (!parts[1].matches("\\d-\\d")) {
+            System.out.println("Invalid input format. Use row-column next to the command (For example: 3-4)");
+            return;
+        }
+
+        String[] coords = parts[1].split("-");
+        int row = Integer.parseInt(coords[0]);
+        int column = Integer.parseInt(coords[1]);
+
+        if (row==8 || row==9) {
+            System.out.println("Invalid input. Rows only go from 0 to 7.");
+            return;
+        }
+
+        String command = parts[0];
+
+        switch(command) {
+            case "clear":
+                board.clear(row, column);
+                break;
+            case "flag":
+                board.flag(row, column);
+                break;
+            case "unflag":
+                board.unflag(row, column);
+                break;
+            default:
+                System.out.println("Invalid input. The only valid commands are: clear - flag - unflag");
+        }
 
     }
 }
