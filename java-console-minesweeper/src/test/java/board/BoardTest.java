abe23b6f30dbddf94d2ee7f1ba3ef94c2f859bc0
franngo <kitofran15@gmail.com>
2026-02-21 19:37:39 -0300

    Se continua el desarrollo del testing de Board.

--- java-console-minesweeper/src/test/java/board/BoardTest.java
+++ java-console-minesweeper/src/test/java/board/BoardTest.java
@@ -17,7 +17,7 @@ public class BoardTest {
     }
 
     @Test
-    public void test() {
+    public void constructor() {
         assertFalse(board.get(0,0).hasBomb());
         assertFalse(board.get(0,1).hasBomb());
         assertFalse(board.get(0,2).hasBomb());
@@ -43,4 +43,52 @@ public class BoardTest {
         }
         assertEquals(10, i);
     }
+
+    @Test
+    public void getActive() {
+        assertTrue(board.getIsActive());
+    }
+
+    @Test
+    public void finishGame() {
+        board.finishGame();
+        assertFalse(board.getIsActive());
+    }
+
+    @Test
+    public void get() {
+        Cell[][] cells = board.getCells();
+        assertEquals(cells[3][5], board.get(3,5));
+    }
+
+    @Test
+    public void flag() {
+        Cell[][] cells = board.getCells();
+
+        Cell cell = mock(Cell.class);
+        cells[4][8] = cell;
+        board.flag(4, 8);
+        verify(cell).flag();
+    }
+
+    @Test
+    public void unflag() {
+        Cell[][] cells = board.getCells();
+
+        Cell cell = mock(Cell.class);
+        cells[4][8] = cell;
+        board.unflag(4, 8);
+        verify(cell).unflag();
+    }
+
+    @Test
+    public void clear() {
+        Cell[][] cells = board.getCells();
+
+        Cell cell = mock(Cell.class);
+        cells[4][8] = cell;
+        board.clear(4, 8);
+        verify(cell).clear(eq(board), any());
+    }
+
 }
