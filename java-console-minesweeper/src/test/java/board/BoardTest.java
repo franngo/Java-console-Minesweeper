68b0a247fa3ea06069bcdfa9c4dd9309a0f1d80d
franngo <kitofran15@gmail.com>
2026-02-21 21:37:45 -0300

    Se completa el testing de la clase Cell.

--- java-console-minesweeper/src/test/java/board/BoardTest.java
+++ java-console-minesweeper/src/test/java/board/BoardTest.java
@@ -5,9 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-import cellState.CellState;
-import cellState.CellStateClear;
-
 public class BoardTest {
     Board board;
 
